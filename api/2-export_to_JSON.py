ca150abb13f53e5cea7916c912622e14126f43e7
Vozudo <jploperatobon@gmail.com>
2022-12-16 00:48:55 -0500

    Task 2 json

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for export data in the CSV format."""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(sys.argv[1])).json()
+
+    list = []
+    for element in todo:
+        aux_dict = {}
+        aux_dict['task'] = data.get('title')
+        aux_dict['completed'] = data.get('completed')
+        aux_dict['username'] = user.get('username')
+        list.append(aux_dict)
+    result_json = {}
+    result_json[sys.argv[1]] = list
+
+    s_json = json.dumps(result_json)
+
+    with open('{}.json'.format(sys.argv[1]), 'w') as f:
+        f.write(s_json)
