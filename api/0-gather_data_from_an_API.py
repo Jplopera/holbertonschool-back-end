081e7e3520c0d12cdeb080262e701be8a8204e56
Vozudo <jploperatobon@gmail.com>
2022-12-15 23:58:35 -0500

    aphabethic order

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module for making a request"""
-import sys
 import requests
+import sys
 if __name__ == "__main__":
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                         .format(sys.argv[1])).json()
