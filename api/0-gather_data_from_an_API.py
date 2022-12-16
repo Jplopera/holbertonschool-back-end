70c05ed8abebc1588cc1cee640ae5e0ce4b07199
Vozudo <jploperatobon@gmail.com>
2022-12-16 00:07:05 -0500

    [Task 0] Update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,18 +2,24 @@
 """Module for making a request"""
 import requests
 import sys
+
+
 if __name__ == "__main__":
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                        .format(sys.argv[1])).json()
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(sys.argv[1])).json()
 
+    task_done = 0
+    total_task = 0
     completed_tasks = []
     for element in todo:
         if element['completed']:
+            task_done += 1
             completed_tasks.append(element['title'])
+        total_task += 1
 
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get('name'), len(completed_tasks), len(todo)))
+        user.get('name'), task_done, total_task))
     for element in completed_tasks:
         print('\t ' + element)
