f20cebccc7cb9836c9c749a1bc31ca18e78072e7
Vozudo <jploperatobon@gmail.com>
2022-12-16 01:25:46 -0500

    Task 3 Update

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -10,17 +10,18 @@ if __name__ == "__main__":
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                         ).json()
 
+    list_todo = list(todo.json)
     list_users = list(user.json)
 
     result_json = {}
     for user in list_users:
 
         list = []
-        for element in todo:
+        for element in list_todo:
             aux_dict = {}
-            aux_dict['task'] = element.get('title')
-            aux_dict['completed'] = element.get('completed')
-            aux_dict['username'] = user.get('username')
+            aux_dict['username'] = user['username']
+            aux_dict['task'] = element['title']
+            aux_dict['completed'] = element['completed']
 
             if user['id'] == element['userId']:
                 list.append(aux_dict)
