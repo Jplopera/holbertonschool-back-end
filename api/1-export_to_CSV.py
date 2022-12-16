b9b76492fa27802e3c7ca9a31d7ac6fb1cca8da7
Vozudo <jploperatobon@gmail.com>
2022-12-16 00:22:43 -0500

    [Task 1] Csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for export data in the CSV format."""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                        .format(sys.argv[1])).json()
+
+    todo_list = list(todo.json())
+    name_list = list(todo.json())
+
+    list = []
+    for element in todo_list:
+        i = []
+        i.append(str(element['userId']))
+        i.append(str(name_list[0]['username']))
+        i.append(str(element['completed']))
+        i.append(str(element['title']))
+        list.append(i)
+    with open("{}.csv".format(sys.argv[1]), 'w', newline='') as file:
+        write = csv.writer(file, quoting=csv.QUOTE_ALL)
+        write.writerows(list)
